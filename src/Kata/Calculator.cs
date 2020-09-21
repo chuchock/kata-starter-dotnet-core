46ab1dbcb02f88fc55efb68d3596e78691817739
Carlos Solorzano <csolorzanoflores@gmail.com>
2020-09-21 14:59:24 -0600

    Ping

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -4,12 +4,20 @@ namespace Kata
     {
         public int Add(string numbers = "")
         {
-            if (numbers.Length == 1)
+            if (numbers.Length == 0)
             {
-                return int.Parse(numbers);
+                return 0;
             }
 
-            return 0;
+            var number = numbers.Split(",");
+            if (number.Length == 1)
+            {
+                return int.Parse(number[0]);
+            }
+            else
+            {
+                return int.Parse(number[0]) + int.Parse(number[1]);
+            }
         }
     }
 }
